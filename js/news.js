6cb1c3992b5abb17cfb1f0a33ad21352fe7a3965
Faye Skeen <fskeen@gmail.com>
2019-06-25 23:14:20 -0700

    Created new menu layout, outlined necessary JS for it, added extra 1000px breakpoint for article container.

--- js/news.js
+++ js/news.js
@@ -5,3 +5,25 @@ const animMenu = document.querySelectorAll('.tab')
 console.log(animMenu)
 
 TweenMax.fromTo(animMenu, 1, {x:20}, {x:0});
+
+
+
+// desktop menu - filter button
+
+/* 
+
+MENU IN
+1. mouse over filter button
+> opacity full, change color
+2. click button
+> color change
+> display: grid;
+> tween menu out in 2 parts, bg then tabs
+> tabs clickable to sort articles (use existing component)
+
+MENU OUT
+1. click filter button again / click outside
+> tabs animate out
+> display: none
+filter slides back and opacity is at .5
+*/
